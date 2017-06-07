a10a7fa6a14fa8ce1ba42a47ee9da195672a20f0
chazmcgrill <charlietaylordev@gmail.com>
2017-06-07 20:05:03 +0100

    Added PHP mailer
    Added php mailer and updated form attributes on the index file. Fixed
    grid issues for the portfolio section

---
+++ php/mailer.php
@@ -0,0 +1,46 @@
+<?php
+
+    // Get the form fields, removes html tags and whitespace.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+
+    // Check the data.
+    if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: http://www.charlietaylordev.com/index.php?success=-1#form");
+        exit;
+    }
+
+    // Set the recipient email address. Update this to YOUR desired email address.
+    $recipient = "charlietaylordev@gmail.com";
+
+    // Set the email subject.
+    $subject = "New contact from $name";
+
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
+
+    // Send the email.
+    mail($recipient, $subject, $email_content, $email_headers);
+
+    // Redirect to the index.html page with success code
+    header("Location: http://www.charlietaylordev.com/index.php?success=1#form");
+
+?>
+
+<!-- place on index page change to php -->
+<!-- <?php
+  if ($_GET['success'] == 1) {
+    echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>"
+  }
+
+  if ($_GET['success'] == 1) {
+    echo "<div class=\"form-messages error\">ops! Something went wrong.</div>"
+  }
+?> -->
