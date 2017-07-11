61bf00da40a97388a264f2534391e4ea4c279149
chazmcgrill <charlietaylordev@gmail.com>
2017-07-11 14:34:33 +0100

    Improved Navigation Scroller
    improved navigation scroller code more compressed and more semantic

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,30 @@
 $('document').ready(function(){
 
-  // navigation scroll
-  $('a[href*="#"]:not([href="#"])').click(function () {
-    if(location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-
-    }
+  // navigation smooth scroll
+  $(function() {
+    smoothScroll(1500);
   });
 
-  // Arrow animation timer
+  function smoothScroll(duration) {
+  	$('a[href^="#"]').on('click', function(event) {
+  	    var target = $( $(this).attr('href') );
+  	    if( target.length ) {
+  	        event.preventDefault();
+  	        $('html, body').animate({
+  	            scrollTop: target.offset().top
+  	        }, duration);
+  	    }
+  	});
+  }
+
+  // arrow animation timer
   var val = 'animated flash',
       arrow = $('.arrow'),
       count = 0;
 
   setInterval(function animTimer() {
 
-    if (arrow.hasClass(val)) { 
+    if (arrow.hasClass(val)) {
       arrow.removeClass(val);
     } else {
       arrow.addClass(val);
