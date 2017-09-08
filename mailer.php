445b6873ac5b8bce1e667e1d5d804e7452a38b91
chazmcgrill <charlietaylordev@gmail.com>
2017-09-08 12:44:22 +0100

    Readded Flash Animation / General Tidy Up and Prefixing
    Re-added the flash animation so the down arrow on header flashes.
    Removed unnecessary files and auto-prefixed through CodeKit.

--- mailer.php
+++ mailer.php
@@ -38,10 +38,10 @@
 
 <?php
   if ($_GET['success'] == 1) {
-    echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>"
+    echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>";
   }
 
   if ($_GET['success'] == -1) {
-    echo "<div class=\"form-messages error\">Oops message sending failed.</div>"
+    echo "<div class=\"form-messages error\">Oops message sending failed.</div>";
   }
 ?>
