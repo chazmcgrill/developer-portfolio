5c3230f009f02b6b727562668268c23bf6e1219f
chazmcgrill <charlietaylordev@gmail.com>
2018-09-25 16:59:09 +0100

    Plumbed in Modal Data

--- includes/modal.php
+++ includes/modal.php
@@ -5,54 +5,20 @@
       <div class="close-button">&times;</div>
     </div>
     <div class="modal-content">
-      <div class="portfolio-item" id="pf0"><img src="./dist/assets/img/portfolio-analogclock.jpg" alt="FCC Camper Leaderboard">
-        <h4>Analog Style Clock</h4>
-        <p>Analog style clock created using vanilla javascript. Makes use of loops within pug and sass to create the clock.</p><a target="_blank" href="https://codepen.io/chazmcgrill/full/mMjVmy/">view on codepen</a><a target="_blank" href="https://github.com/chazmcgrill/analog-clock">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf1">
-        <h4>Javascript Calculator</h4><img src="./dist/assets/img/portfolio-calc.jpg" alt="FCC Camper Leaderboard">
-        <p>Simple arithmetic calculator created using javascript. Includes percentage, decimals and negate functionality.</p><a target="_blank" href="https://codepen.io/chazmcgrill/full/QgKvLZ/">view on codepen</a><a target="_blank" href="https://github.com/chazmcgrill/calculator">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf2">
-        <h4>Hurricane Charlie Website</h4><img src="./dist/assets/img/portfolio-hurricanecharlie.jpg" alt="Hurricane Charlie">
-        <p>Personal illustration portfolio contains gallery, contact and shop. key technologies React, React-router, Node, Express and CSS grid. </p><a target="_blank" href="http://www.hurricanecharlie.co.uk">view website</a><a target="_blank" href="https://github.com/chazmcgrill/hurricane-charlie-website">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf3">
-        <h4>React Pizza App</h4><img src="./dist/assets/img/portfolio-pizzabox.jpg" alt="React Pizza App">
-        <p>React app for storing pizza recipes, functionality for adding new recipes and editing or deleting existing recipes. Info is stored locally so when browser is reloaded the data persists.</p><a target="_blank" href="https://codepen.io/chazmcgrill/full/pdOjGm/">view on codepen</a><a target="_blank" href="https://github.com/chazmcgrill/recipe-box">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf4">
-        <h4>Pomodoro Timer</h4><img src="./dist/assets/img/portfolio-pomodoro.jpg" alt="FCC Camper Leaderboard">
-        <p>Pomodoro timer for an app for improving time management and workflow. Animated and coded using css and javascript.</p><a target="_blank" href="https://codepen.io/chazmcgrill/full/rwGbVR/">view on codepen</a><a target="_blank" href="https://github.com/chazmcgrill/pomodoro-clock">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf5">
-        <h4>Quote Machine</h4><img src="./dist/assets/img/portfolio-quote.jpg" alt="Quote Machine">
-        <p>App that generates random quotes collected from an external API. New quotes can be generated and can be shared on twitter.</p><a target="_blank" href="https://codepen.io/chazmcgrill/full/jBmygM/">view on codepen</a><a target="_blank" href="https://github.com/chazmcgrill/quote-machine">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf6">
-        <h4>React Life App</h4><img src="./dist/assets/img/portfolio-life.jpg" alt="React Life App">
-        <p>React project based on the cellular automaton 'Life' devised by mathematician John Conway.</p><a target="_blank" href="https://codepen.io/chazmcgrill/full/QOoOaj/">view on codepen</a><a target="_blank" href="https://github.com/chazmcgrill/game-of-life">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf7">
-        <h4>Bugsweeper</h4><img src="./dist/assets/img/portfolio-bugsweeper.jpg" alt="Bugsweeper">
-        <p>Recreation of the classic windows desktop game minesweeper. Built using React javascript, it utilises an 'flood fill' algorithm.</p><a target="_blank" href="https://charlietaylorcoder.com/bugsweeper">view app</a><a target="_blank" href="https://github.com/chazmcgrill/bugsweeper">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf8">
-        <h4>TicTacToe</h4><img src="./dist/assets/img/portfolio-tictactoe.jpg" alt="TicTacToe">
-        <p>Game of noughts and crosses human vs computer. Utilises a minimax algorithm to make the computer unbeatable.</p><a target="_blank" href="https://codepen.io/chazmcgrill/full/EwyGOG/">view on codepen</a><a target="_blank" href="https://github.com/chazmcgrill/tic-tac-toe">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf9">
-        <h4>TwitchBox</h4><img src="./dist/assets/img/portfolio-twitchtv.jpg" alt="TwitchBox">
-        <p>Application for showing TwitchTV channel data using their API. Shows channel status and links to the video stream.</p><a target="_blank" href="https://codepen.io/chazmcgrill/full/oWpVOG/">view on codepen</a><a target="_blank" href="https://github.com/chazmcgrill/twitch-tv">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf10">
-        <h4>Weather App</h4><img src="./dist/assets/img/portfolio-weather.jpg" alt="FCC Camper Leaderboard">
-        <p>Provides current weather status for your current location. Uses geolocation and uses two external API's for location and weather.</p><a target="_blank" href="https://codepen.io/chazmcgrill/full/BRKaPB/">view on codepen</a><a target="_blank" href="https://github.com/chazmcgrill/weather">view on github</a>
-      </div>
-      <div class="portfolio-item filtered" id="pf11">
-        <h4>Coinage</h4><img src="./dist/assets/img/portfolio-coinage.jpg" alt="Coinage">
-        <p>Simple cryptocurrency price tracking app. Built using react test driven development. Data is pulled from cryptocompare api via multiple AJAX requests.</p><a target="_blank" href="https://charlietaylorcoder.com/coinage">view app</a><a target="_blank" href="https://github.com/chazmcgrill/coinage">view on github</a>
-      </div>
+
+      <?php foreach($portfolio as $modal_item) { ?>
+        <div class="portfolio-item" id="<?php echo $modal_item['id']; ?>">
+          <img src="./dist/assets/img/<?php echo $modal_item['img']; ?>" alt="<?php echo $modal_item['title']; ?>">
+          <h4><?php echo $modal_item['title']; ?></h4>
+          <p><?php echo $modal_item['text']; ?></p>
+          <?php foreach($modal_item['links'] as $link) { ?>
+            <a target="_blank" href="<?php echo $site_urls[$link['site']] . $link['url']; ?>">
+              view on <?php echo $link['site'] ?>
+            </a>
+          <?php } ?>
+        </div>
+      <?php } ?>
+    
     </div>
     <div class="modal-footer">
       <button class="modal-prev"><i class="fa fa-arrow-left"></i></button>
