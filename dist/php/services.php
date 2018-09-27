b6952a0b43851a154e40e07e6d5f070b4cdd639f
chazmcgrill <charlietaylordev@gmail.com>
2018-09-27 11:00:15 +0100

    Services Styling Restructure

--- dist/php/services.php
+++ dist/php/services.php
@@ -1,13 +1,13 @@
 <section class="section-services" id="services">
-  <div class="row">
+  <div class="row row_centered">
     <h2>Services</h2>
   </div>
-  <div class="row">
-    <div class="services-container">
+  <div class="row row_spaced">
+    <!-- <div class="services-container"> -->
       
       <?php foreach($services as $service) { ?>
         
-        <div class="services-box">
+        <div class="col services-box">
           <i class="fas <?php echo $service['icon']; ?> fa-3x" aria-hidden="true"></i>
           <h3><?php echo $service['title']; ?></h3>
           <p><?php echo $service['text']; ?></p>
@@ -15,7 +15,7 @@
       
       <?php } ?>
 
-    </div>
+    <!-- </div> -->
   </div>
 </section>
         
