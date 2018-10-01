edfd1592d4b2b9c18fbf3c4b6fd6dc1ed0b048c7
chazmcgrill <charlietaylordev@gmail.com>
2018-10-01 17:24:22 +0100

    Improved Skills Styling

--- dist/php/skills.php
+++ dist/php/skills.php
@@ -1,14 +1,11 @@
 <section class="section-skills" id="skills">
+  <h3>Technologies used:</h3>
   <div class="row">
-    <h3>Technologies used:</h3>
-  </div>
-  <div class="row">
-    <div class="col col_full skills-box">
+    <div class="col col_full skills">
 
       <?php foreach ($skills as $skill) { ?>
-        <div class="icon icon-<?php echo $skill; ?>" tool-title="<?php echo $skill; ?>"></div>
-      <?php 
-    } ?>
+        <div class="skill skill--<?php echo $skill; ?>" tool-title="<?php echo $skill; ?>"></div>
+      <?php } ?>
       
     </div>
   </div>
