4013680445f55256ae264b833707abf65a65afea
chazmcgrill <charlietaylorcoder@gmail.com>
2019-11-13 10:10:38 +0000

    Adds portfolio section

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <Bio v-bind:social-icons="socialIcons" />
       <Services v-bind:services="services" />
       <Skills v-bind:skills="skills" />
+      <Portfolio v-bind:projects="projects" />
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import Header from './components/Header.vue'
 import Bio from './components/Bio.vue'
 import Services from './components/Services.vue'
 import Skills from './components/Skills.vue'
+import Portfolio from './components/Portfolio.vue'
 
 export default {
     name: 'app',
@@ -19,7 +21,8 @@ export default {
         Header,
         Bio,
         Services,
-        Skills
+        Skills,
+        Portfolio,
     },
     data() {
         return {
@@ -67,6 +70,128 @@ export default {
                 'illustrator',
                 'photoshop'
             ],
+            projects: [
+                {
+                    img: 'portfolio-analogclock.jpg', 
+                    title: 'Analog Style Clock',
+                    id: 'pf0',
+                    text: 'Analog style clock created using vanilla javascript. Makes use of loops within pug and sass to create the clock.',
+                    links: [
+                        { 'site': 'codepen', 'url': 'mMjVmy/' },
+                        { 'site': 'github', 'url': 'analog-clock' }
+                    ]
+                },
+                {
+                    img: 'portfolio-calc.jpg', 
+                    title: 'Javascript Calculator',
+                    id: 'pf1',
+                    text: 'Simple arithmetic calculator created using javascript. Includes percentage, decimals and negate functionality.',
+                    links: [
+                        { 'site': 'codepen', 'url': 'QgKvLZ/' },
+                        { 'site': 'github', 'url': 'calculator' }
+                    ]
+                },
+                {
+                    img: 'portfolio-hurricanecharlie.jpg', 
+                    title: 'Hurricane Charlie Website',
+                    id: 'pf2',
+                    text: 'Personal illustration portfolio contains gallery, contact and shop. key technologies React, React-router, Node, Express and CSS grid.',
+                    links: [
+                        { 'site': 'website', 'url': 'https://www.hurricanecharlie.co.uk' },
+                        { 'site': 'github', 'url': 'hurricane-charlie-website' }
+                    ]
+                },
+                {
+                    img: 'portfolio-pizzabox.jpg', 
+                    title: 'React Pizza Box',
+                    id: 'pf3',
+                    text: 'React app for storing pizza recipes, functionality for adding new recipes and editing or deleting existing recipes. Info is stored locally so when browser is reloaded the data persists.',
+                    links: [
+                        { 'site': 'codepen', 'url': 'pdOjGm/' },
+                        { 'site': 'github', 'url': 'recipe-box' }
+                    ]
+                },
+                {
+                    img: 'portfolio-pomodoro.jpg', 
+                    title: 'Pomodoro Timer',
+                    id: 'pf4',
+                    text: 'Pomodoro timer for an app for improving time management and workflow. Animated and coded using css and javascript.',
+                    links: [
+                        { 'site': 'codepen', 'url': 'rwGbVR/' },
+                        { 'site': 'github', 'url': 'pomodoro-clock' }
+                    ]
+                },
+                {
+                    img: 'portfolio-quote.jpg', 
+                    title: 'Quote Machine',
+                    id: 'pf5',
+                    text: 'App that generates random quotes collected from an external API. New quotes can be generated and can be shared on twitter.',
+                    links: [
+                    { 'site': 'codepen', 'url': 'jBmygM/' },
+                    { 'site': 'github', 'url': 'quote-machine' }
+                    ]
+                },
+                {
+                    img: 'portfolio-life.jpg', 
+                    title: 'Life App',
+                    id: 'pf6',
+                    text: 'React project based on the cellular automaton \'Life\' devised by mathematician John Conway.',
+                    links: [
+                        { 'site': 'codepen', 'url': 'QOoOaj/' },
+                        { 'site': 'github', 'url': 'game-of-life' }
+                    ]
+                },
+                {
+                    img: 'portfolio-bugsweeper.jpg', 
+                    title: 'Bugsweeper',
+                    id: 'pf7',
+                    text: 'Recreation of the classic windows desktop game minesweeper. Built using React javascript, it utilises an \'flood fill\' algorithm.', 
+                    links: [
+                        { 'site': 'website', 'url': 'https://charlietaylorcoder.com/bugsweeper' },
+                        { 'site': 'github', 'url': 'bugsweeper' }
+                    ]
+                },
+                {
+                    img: 'portfolio-tictactoe.jpg', 
+                    title: 'Tictactoe Game',
+                    id: 'pf8',
+                    text: 'Game of noughts and crosses human vs computer. Utilises a minimax algorithm to make the computer unbeatable.',
+                    links: [
+                        { 'site': 'codepen', 'url': 'EwyGOG/' },
+                        { 'site': 'github', 'url': 'tic-tac-toe' }
+                    ]
+                },
+                {
+                    img: 'portfolio-twitchtv.jpg', 
+                    title: 'TwitchBox',
+                    id: 'pf9',
+                    text: 'Application for showing TwitchTV channel data using their API. Shows channel status and links to the video stream.',
+                    links: [
+                        { 'site': 'codepen', 'url': 'oWpVOG/' },
+                        { 'site': 'github', 'url': 'twitch-tv' }
+                    ]
+                },
+                {
+                    img: 'portfolio-weather.jpg', 
+                    title: 'Weather App',
+                    id: 'pf10',
+                    text: 'Provides current weather status for your current location. Uses geolocation and uses two external API\'s for location and weather.',
+                    links: [
+                        { 'site': 'codepen', 'url': 'BRKaPB/' },
+                        { 'site': 'github', 'url': 'weather' }
+                    ]
+                },
+                {
+                    img: 'portfolio-coinage.jpg', 
+                    title: 'Coinage App',
+                    id: 'pf11',
+                    text: 'Simple cryptocurrency price tracking app. Built using react test driven development. Data is pulled from cryptocompare api via multiple AJAX requests.',
+                    links: [
+                        { 'site': 'website', 'url': 'https://charlietaylorcoder.com/coinage' },
+                        { 'site': 'github', 'url': 'coinage' }
+                    ]
+                }
+            ]
         }
     }
 }
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,92 @@
+<template>
+    <section class="section-portfolio" id="portfolio">
+        <div class="row row_centered">
+            <h2>Portfolio</h2>
+        </div>
+        <div class="row row_centered">
+            <div class="projects">
+
+                <div v-for="project in projects" :key="project.title" class="project" :id="project.id" :style="{backgroundImage: 'url(' + require('../assets/'+ project.img) + ')' }">
+                    <div class="project--overlay"></div>
+                    <h3 class="project--title">{{project.title}}</h3>
+                </div>
+            
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Portfolio',
+    props: ['projects']
+}
+</script>
+
+<style lang="sass" scoped>
+
+.section-portfolio
+  // overflow: auto
+
+.projects
+  width: 100%
+  display: grid
+  grid-template-columns: repeat(4, 1fr)
+  grid-auto-rows: 1fr
+
+  @media (max-width: 760px)
+    grid-template-columns: repeat(2, 1fr)
+  @media (max-width: 480px)
+    grid-template-columns: repeat(1, 1fr)
+
+  &::before
+    content: ""
+    width: 0
+    padding-bottom: 100%
+    grid-row: 1 / 1
+    grid-column: 1 / 1
+
+.project
+  position: relative
+  background:
+    size: cover
+    position: center
+  cursor: pointer
+  display: flex
+  justify-content: center
+  align-items: center
+  text-align: center
+  padding: 30px
+  
+  &:first-child
+    grid-row: 1 / 1
+    grid-column: 1 / 1
+
+  &--overlay
+    position: absolute
+    top: 0
+    left: 0
+    background: black
+    height: 100%
+    width: 100%
+    opacity: 0
+    transition: opacity 0.3s ease-in-out
+    pointer-events: none
+
+  &--title
+    opacity: 0
+    color: white
+    font-size: 1rem
+    z-index: 1
+    transform: scale(0.1)
+    transition: all 0.3s ease-in-out
+    pointer-events: none
+
+  &:hover .project--title
+    opacity: 1
+    transform: scale(1)
+
+  &:hover .project--overlay
+    opacity: 0.8
+
+</style>
