f02d76c28225444d451e96f3a34311d30d94c3c9
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-11-05 17:35:35 +0000

    feat: adds icon setup and starts layout css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import styles from '../styles/Navbar.module.sass';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 const Navbar = () => {
     // TODO: use routing to determine which page we are on
     return (
         <div className={styles.navbar}>
-            <h3>Charlie Taylor</h3>
-
-            <div>
-                <span className={styles.nav}>about</span>
-                <span className={styles.nav}>projects</span>
-                <span className={styles.nav}>contact</span>
+            <div className={styles.title}>
+                <h3>Charlie Taylor</h3>
+                <h4>Full Stack Developer</h4>
             </div>
 
-            <span>light/dark</span>
+            <div>
+                <span className={styles.navItem}>about</span>
+                <span className={styles.navItem}>projects</span>
+                <span className={styles.navItem}>contact</span>
+            </div>
+
+            <div className={styles.icons}>
+                <FontAwesomeIcon icon={faTwitter} />
+                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faLinkedin} />
+            </div>
         </div>
     );
 };
