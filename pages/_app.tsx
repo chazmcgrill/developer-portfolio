137144c6c37ac0d3d0776bcb3701483923733fd8
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-11-06 12:04:02 +0000

    feat: adds font setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.sass';
 import type { AppProps } from 'next/app';
+import { Inter } from '@next/font/google';
+
+const inter = Inter();
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <main className={inter.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
 
 export default MyApp;
