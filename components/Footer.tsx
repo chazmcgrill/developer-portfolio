fd1c7aee5827841b9d79fec81b989ab8c8f39bee
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-11-06 13:15:31 +0000

    feat: adds icons to footer and modularises them for reuse with navbar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from '../styles/Footer.module.sass';
+import SocialIcons from './SocialIcons';
 
 const Footer = () => (
     <div className={styles.footer}>
         <hr />
         <h4>Charlie Taylor</h4>
         <p>Lorem ipsum, dolor sit amet.</p>
+        <SocialIcons />
     </div>
 );
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import styles from '../styles/Navbar.module.sass';
+import SocialIcons from './SocialIcons';
 
 const getNavItemClass = (pathname: string, currentPathName: string) => {
     if (pathname === currentPathName) return styles.selectedNavItem;
@@ -34,17 +33,7 @@ const Navbar = () => {
                 </Link>
             </div>
 
-            <div className={styles.icons}>
-                <a href="https://twitter.com/charlietcoder" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon icon={faTwitter} />
-                </a>
-                <a href="https://github.com/chazmcgrill" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon icon={faGithub} />
-                </a>
-                <a href="https://uk.linkedin.com/in/charliejbtaylor" target="_blank" rel="noreferrer">
-                    <FontAwesomeIcon icon={faLinkedin} />
-                </a>
-            </div>
+            <SocialIcons />
         </div>
     );
 };
