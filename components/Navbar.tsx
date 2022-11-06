a835eeffcbab7146861b97a1a9d5b6a4103b8f5f
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-11-06 12:48:17 +0000

    feat: navbar component (#50)
    * nextjs boilerplate (#44)
    
    * adds nextjs boilerplate
    
    * updates node version on ci
    
    * ci: replaces deprecated node image
    
    * ci: fix dist folder in ci build
    
    * Static testing setup (#46)
    
    * style: adds prettier and formatting scripts
    
    * chore: adds check types script
    
    * chore: adds validation script
    
    * chore: adds pre-commit static testing
    
    * Site component structure (#47)
    
    * feature: adds basis for index page and page container
    
    * chore: adds node required version
    
    * feat: adds nav bar with basic styling & add sass setup
    
    * chore: remove eslint cache from remote
    
    * chore: adds eslintcache to git ignore
    
    * feat: adds structure for footer and homepage
    
    * chore: adds github PR template
    
    * chore: adds github PR template
    
    * feat: adds icon setup and starts layout css
    
    * chore: updates next to v13 (#48)
    
    * feat: adds font setup
    
    * fix: fixes font setup by adding subset
    
    * feat: adds nav selected route highlighting and social links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 import styles from '../styles/Navbar.module.sass';
 
+const getNavItemClass = (pathname: string, currentPathName: string) => {
+    if (pathname === currentPathName) return styles.selectedNavItem;
+    return styles.navItem;
+};
+
 const Navbar = () => {
-    // TODO: use routing to determine which page we are on
+    const { pathname } = useRouter();
+
     return (
         <div className={styles.navbar}>
-            <h3>Charlie Taylor</h3>
-
-            <div>
-                <span className={styles.nav}>about</span>
-                <span className={styles.nav}>projects</span>
-                <span className={styles.nav}>contact</span>
+            <div className={styles.title}>
+                <Link href="/">
+                    <h3>Charlie Taylor</h3>
+                </Link>
+                <h4 className={styles.jobDescription}>Full Stack Developer</h4>
             </div>
 
-            <span>light/dark</span>
+            <div>
+                <Link className={getNavItemClass('about', pathname)} href="/about">
+                    About
+                </Link>
+                <Link className={getNavItemClass('projects', pathname)} href="/projects">
+                    Projects
+                </Link>
+                <Link className={getNavItemClass('contact', pathname)} href="/contact">
+                    Contact
+                </Link>
+            </div>
+
+            <div className={styles.icons}>
+                <a href="https://twitter.com/charlietcoder" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faTwitter} />
+                </a>
+                <a href="https://github.com/chazmcgrill" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a href="https://uk.linkedin.com/in/charliejbtaylor" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+            </div>
         </div>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,10 @@ import HomePage from '../components/HomePage';
 import PageContainer from '../components/PageContainer';
 // import styles from '../styles/Home.module.css';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 const Home: NextPage = () => {
     return (
         <PageContainer>
