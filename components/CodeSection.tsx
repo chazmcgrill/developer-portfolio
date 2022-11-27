767f23a9b30e3671d8a73c5fba6be9b6999aab9f
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-11-27 19:17:45 +0000

    feat: adds base styling for code repos

--- components/CodeSection.tsx
+++ components/CodeSection.tsx
@@ -1,10 +1,18 @@
 import { spawn } from 'child_process';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import styles from '../styles/CodeSection.module.sass';
 import SectionHeader from './SectionHeader';
 
+interface GithubRepo {
+    id: number;
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 const useGithubRepositories = () => {
-    const [repositories, setRepositories] = useState<any>([]);
+    const [repositories, setRepositories] = useState<GithubRepo[]>([]);
 
     useEffect(() => {
         const fetchRepositories = async () => {
@@ -23,15 +31,27 @@ const useGithubRepositories = () => {
     return repositories;
 };
 
+const CodeRepoItem = ({ repository }: { repository: GithubRepo }) => {
+    return (
+        <div>
+            <h4>{repository.name}</h4>
+            <p>{repository.description}</p>
+            <Link href={repository.html_url}>view repo</Link>
+        </div>
+    );
+};
+
 const CodeSection = () => {
     const repositories = useGithubRepositories();
     console.log('repositories', repositories);
     return (
         <section className={styles.codeSection}>
             <SectionHeader title="Code" buttonText="view on github" buttonHref="https://github.com/chazmcgrill?tab=repositories" />
-            {repositories.map((repository: any) => (
-                <span key={repository.name}>{repository.name}</span>
-            ))}
+            <div className={styles.codeRepoGrid}>
+                {repositories.map((repository) => (
+                    <CodeRepoItem key={repository.id} repository={repository} />
+                ))}
+            </div>
         </section>
     );
 };
