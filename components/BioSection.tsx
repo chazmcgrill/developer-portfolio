228d7ab867f431a62d50ca7b95c46b9ce24bc2e1
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-12-30 17:29:25 +0000

    feat: responsive layout styling (#59)
    * feat: bio section responsive layout
    
    * feat: projects section responsive layout
    
    * feat: code section responsive layout
    
    * feat: footer responsive layout and some fine tuning on sections

--- components/BioSection.tsx
+++ components/BioSection.tsx
@@ -5,14 +5,14 @@ import config from '../utils/config';
 const BioSection = () => {
     return (
         <section className={styles.bioSection}>
+            <div>
+                <div className={styles.bioImage}></div>
+            </div>
             <div className={styles.bioSectionText}>
                 <h1>Innovative full stack developer with a mix of technical and creative skills.</h1>
                 <a href={config.twitterDirectMessageHref} target="_blank" rel="noreferrer">
                     <button>Talk to me</button>
                 </a>
-            </div>
-            <div>
-                <div className={styles.bioImage}></div>
             </div>
         </section>
     );
