06ab84cb3ad2d5ab3f0362d26629f8dc976c17e4
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-12-30 11:43:50 +0000

    feat: adds real text for bio and footer statements

--- components/BioSection.tsx
+++ components/BioSection.tsx
@@ -5,7 +5,7 @@ const BioSection = () => {
     return (
         <section className={styles.bioSection}>
             <div className={styles.bioSectionText}>
-                <h1>Fuga beatae ducimus dolor, ipsum molestiae a magnam. Modi, beatae alias.</h1>
+                <h1>Innovative full stack developer with a mix of technical and creative skills.</h1>
                 <button>Talk to me</button>
             </div>
             <div>
