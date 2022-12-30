df3e987f2ace8373e601a4ddc788c86e77c18b12
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-12-30 12:13:17 +0000

    refactor: moves projects info into config and makes project a separate component

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -3,32 +3,29 @@ import styles from '../styles/ProjectsSection.module.sass';
 import config from '../utils/config';
 import SectionHeader from './SectionHeader';
 
+interface ProjectItemProps {
+    title: string;
+    description: string;
+    height: number;
+}
+
+const ProjectItem = ({ title, description, height }: ProjectItemProps) => (
+    <div className={styles.projectItem}>
+        <div className={styles.projecItemImage} style={{ height }}></div>
+        <h3>{title}</h3>
+        <small>{description}</small>
+    </div>
+);
+
 const ProjectsSection = () => {
     return (
         <section className={styles.projectsSection}>
             <SectionHeader title="Projects" buttonText={config.features.pages ? 'view all' : undefined} buttonHref="/projects" />
 
             <div className={styles.projectGrid}>
-                <div className={styles.projectItem}>
-                    <div className={styles.projecItemImage} style={{ height: 500 }}></div>
-                    <h3>Project title 1</h3>
-                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
-                </div>
-                <div className={styles.projectItem}>
-                    <div className={styles.projecItemImage} style={{ height: 400 }}></div>
-                    <h3>Project title 2</h3>
-                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
-                </div>
-                <div className={styles.projectItem}>
-                    <div className={styles.projecItemImage} style={{ height: 400 }}></div>
-                    <h3>Project title 3</h3>
-                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
-                </div>
-                <div className={styles.projectItem}>
-                    <div className={styles.projecItemImage} style={{ height: 500 }}></div>
-                    <h3>Project title 4</h3>
-                    <small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
-                </div>
+                {config.projects.map((project) => (
+                    <ProjectItem key={project.id} {...project} />
+                ))}
             </div>
         </section>
     );
--- utils/config.ts
+++ utils/config.ts
@@ -2,6 +2,12 @@ const isTruthy = (envVariable?: string) => envVariable === 'true';
 
 const config = {
     twitterDirectMessageHref: 'https://twitter.com/messages/compose?recipient_id=828212805992067072',
+    projects: [
+        { id: 1, title: 'Project title 1', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', height: 500 },
+        { id: 2, title: 'Project title 2', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', height: 400 },
+        { id: 3, title: 'Project title 3', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', height: 400 },
+        { id: 4, title: 'Project title 4', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', height: 500 },
+    ],
     features: {
         pages: isTruthy(process.env.NEXT_PUBLIC_FEATURE_PAGES),
     },
