a22b4f9d6efc92e85e10b86a925e7423682dfc0b
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2022-12-30 12:00:17 +0000

    feat: adds twitter direct message link for bio button

--- utils/config.ts
+++ utils/config.ts
@@ -1,6 +1,7 @@
 const isTruthy = (envVariable?: string) => envVariable === 'true';
 
 const config = {
+    twitterDirectMessageHref: 'https://twitter.com/messages/compose?recipient_id=828212805992067072',
     features: {
         pages: isTruthy(process.env.NEXT_PUBLIC_FEATURE_PAGES),
     },
