5b5c5ccb166f898e9d4317d46fa45be874038679
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2023-05-13 09:44:59 +0100

    fix: amends twitter handle

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/SocialIcons.module.sass';
 
 const SocialIcons = () => (
     <div className={styles.icons}>
-        <a href="https://twitter.com/charlietcoder" target="_blank" rel="noreferrer">
+        <a href="https://twitter.com/charlietdev" target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faTwitter} />
         </a>
         <a href="https://github.com/chazmcgrill" target="_blank" rel="noreferrer">
