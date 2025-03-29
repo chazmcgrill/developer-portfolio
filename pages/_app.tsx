cb44920797b775908aafd1907492e9023a63d4bc
Charlie Taylor <charlie.taylor@smart-buildings.co.uk>
2025-03-29 18:29:05 +0000

    fix: npm update and audit fixes (#62)
    * chore: adds nvm version and sets updated engines
    
    * fix: npm audit fixes for non breaking changes
    
    * chore: updates can i use browser list
    
    * fix: updates deprecated next font
    
    * chore: eof
    
    * ci: fix actions versioning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.sass';
 import type { AppProps } from 'next/app';
-import { Inter } from '@next/font/google';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({
     weight: ['200', '300', '400'],
